2be4c8856aacfe061b4995a241229abd4e8778e7
VladGlznko <162557894+VladGlznko@users.noreply.github.com>
2024-04-16 09:16:08 +0300

    Add files via upload

--- lab2/ThreadSumJava-master/src/com/company/Main.java
+++ lab2/ThreadSumJava-master/src/com/company/Main.java
@@ -5,10 +5,21 @@ public class Main {
         int dim = 10000;
         int threadNum = 12;
         ArrClass arrClass = new ArrClass(dim, threadNum);
-        int[] result = arrClass.partMin(0,dim);
+        
+     
+        long startTime = System.nanoTime();
+        
+        int[] result = arrClass.partMin(0, dim);
         System.out.println("Min num: " + result[0] + " Index: " + result[1]);
 
         int[] resultThread = arrClass.threadMin();
         System.out.println("Min num: " + resultThread[0] + " Index: " + resultThread[1]);
+        
+ 
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        double seconds = (double) elapsedTime / 1_000_000_000.0;
+        
+        System.out.println("Execution Time: " + seconds + " seconds");
     }
 }
